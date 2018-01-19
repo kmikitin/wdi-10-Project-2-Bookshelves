a13ae2c8007bf9cc3e1774a0d6bfb5126190f3b2
Kait Mikitin <kait.mikitin@gmail.com>
2018-01-19 15:27:19 -0600

    basic user model completed

--- models/users.js
+++ models/users.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+	firstName: String,
+	lastName: String,
+	email: String,
+	location: String,
+	username: String,
+	password: String,
+	genres: [String],
+	bookshelves: {}
 
 })
 
