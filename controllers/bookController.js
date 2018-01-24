7042138711780ac34c252fb7123b5feca7e7ec37
Kait Mikitin <kait.mikitin@gmail.com>
2018-01-23 19:29:29 -0600

    edited some book routes, slight changes to views

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -4,15 +4,23 @@ const router = express.Router();
 // MODEL
 const Book = require('../models/books.js')
 
-// serve the user the search page for books
+// allow the user to see all public bookshelves and puruse
 router.get('/', (req, res) => {
+	res.render('books/index.ejs')
+})
+
+
+// serve the user the search page for books
+router.get('/search', (req, res) => {
 	res.render('books/search.ejs')
 })
 
 // show the user the result of their search
+// SHOULD THIS BE A POST ROUTE?
 router.get('/result', (req, res) => {
 	res.render('books/result.ejs')
 })
+
 
 // show the user information on the book they clicked on
 //<!-- THIS SHOULD BE CHANGED TO A VARIABLE ROUTE ONCE WE HAVE OAUTH WORKING -->
